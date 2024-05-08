37b994b7d9247759c57330258b98fb1aab1106a2
FernandoH <ffhf.1820@gmail.com>
2024-05-08 00:32:34 -0300

    UPDATE: Golang net http

--- GolangTutorial/main.go
+++ GolangTutorial/main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Empezar el CLI")
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /notas", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Obteniendo todas las notas"))
+	})
+
+	router.HandleFunc("GET /notas/{id}", func(w http.ResponseWriter, r *http.Request) {
+		idStr := r.PathValue("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "Invalid to parse id", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "notas by id: %d", id)
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	log.Println("Listening on port: ", server.Addr)
+
+	server.ListenAndServe()
 }
---
+++ GolangTutorial/main_cli.go
@@ -0,0 +1,9 @@
+package main_cli
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Empezar el CLI")
+}
