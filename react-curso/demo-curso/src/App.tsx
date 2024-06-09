9b3a30bb7c35a3092ce8be8e26c68248ced955ca
FernandoH <ffhf.1820@gmail.com>
2024-06-09 20:08:08 -0300

    Clase 6-7-8 Curso de React

--- react-curso/demo-curso/src/App.tsx
+++ react-curso/demo-curso/src/App.tsx
@@ -1,32 +1,29 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+interface User {
+  name: string;
+  age: number;
+}
+
+export const App = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  function AddNumber() {
+    setUser({
+      name: "Pepe",
+      age: 29,
+    });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1> Mi Proyecto </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        {user?.name} {user?.age}
+      </h1>
+      <button onClick={() => AddNumber()}>Aumentar</button>
     </>
   );
-}
+};
 
 export default App;
---
+++ react-curso/demo-curso/src/constants.ts
@@ -0,0 +1,7 @@
+export const fruits = [
+    'Naranja','Manzana','Piña'
+]
+
+export const numbers = [
+    1,2,3,4
+]
